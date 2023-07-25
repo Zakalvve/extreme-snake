3fe0d9f5fd8cb69d19527508c4b451037e45dcb6
Fohristiwhirl-Skaffen-Amtiskaw <112807626+Fohristiwhirl-Skaffen-Amtiskaw@users.noreply.github.com>
2023-07-25 18:07:49 +0100

    changed the data associated with a change in score

--- Assets/Scripts/Game/Data/EventArgs/ScoreEventArgs.cs
+++ Assets/Scripts/Game/Data/EventArgs/ScoreEventArgs.cs
@@ -1,22 +1,30 @@
-﻿using System;
+﻿using ExtremeSnake.Game.UI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace ExtremeSnake.Game.Data
 {
     public class ScoreEventArgs : EventArgs
     {
         public string SnakeUUID { get; }
-        public int Points { get; }
+        public int PointsTotal { get; }
+        public int PointsChange { get; }
+        public bool MultiplierChanged { get; }
+        public Color TextColor { get; }
+        public SnakeScore Score { get; set; } = null;
+        public MultiplierGradients MultiplierColors { get; set; }
 
-        public SnakeScore Score { get; set; }
-
-        public ScoreEventArgs(string snakeUUID, int points) {
+        public ScoreEventArgs(string snakeUUID, int pointsTotal, int pointsGained, bool multiplierChanged, Color textColor) {
             SnakeUUID = snakeUUID;
-            Points = points;
+            PointsTotal = pointsTotal;
+            PointsChange = pointsGained;
+            MultiplierChanged = multiplierChanged;
+            TextColor = textColor;
         }
-        public ScoreEventArgs(string snakeUUID) : this(snakeUUID,0) { }
+        public ScoreEventArgs(string snakeUUID, int pointsGained) : this(snakeUUID,0, pointsGained, false, Color.white) { }
     }
 }
