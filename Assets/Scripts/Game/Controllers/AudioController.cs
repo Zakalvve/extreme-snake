33be9e8ba49060deca83bd47f58b2900319aea4b
Fohristiwhirl-Skaffen-Amtiskaw <112807626+Fohristiwhirl-Skaffen-Amtiskaw@users.noreply.github.com>
2023-07-25 18:00:42 +0100

    re worked the audio controller so that it can handle full state loops

--- Assets/Scripts/Game/Controllers/AudioController.cs
+++ Assets/Scripts/Game/Controllers/AudioController.cs
@@ -1,4 +1,5 @@
 using ExtremeSnake.Game;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,22 +8,30 @@ using UnityEngine;
 public class AudioController : MonoBehaviour
 {
     public AudioSource Source;
+    private List<Action> Subscriptions = new List<Action>();
     private void Awake() {
         Source = GetComponent<AudioSource>();
     }
 
     private void Start() {
-        GameManager.Instance.GameEmitter.Subscribe("OnVolumeChanged",SetVolume);
-        GameManager.Instance.GameEmitter.Subscribe("OnPlayMusicChanged",SetMute);
+        Initialize();
+    }
+
+    public void Initialize() {
+        if (Subscriptions.Count > 0) {
+            Subscriptions.ForEach(unsub => unsub());
+        }
+        Subscriptions.Add(GameManager.Instance.GameEmitter.Subscribe("OnVolumeChanged",SetVolume));
+        Subscriptions.Add(GameManager.Instance.GameEmitter.Subscribe("OnPlayMusicChanged",SetMute));
         SetVolume(this);
         SetMute(this);
     }
 
     private void SetVolume(object sender) {
-        Source.volume = GameManager.Instance.Settings.Volume/100f;
+        Source.volume = GameManager.Instance.Settings.AudioSettings.Volume/100f;
     }
 
     private void SetMute(object sender) {
-        Source.mute = !GameManager.Instance.Settings.PlayMusic;
+        Source.mute = !GameManager.Instance.Settings.AudioSettings.PlayMusic;
     }
 }
