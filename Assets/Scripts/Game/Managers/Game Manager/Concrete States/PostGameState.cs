a8cb616a902564c96e2d6e7e3bf669cd1f37e0be
Fohristiwhirl-Skaffen-Amtiskaw <112807626+Fohristiwhirl-Skaffen-Amtiskaw@users.noreply.github.com>
2023-07-25 18:16:48 +0100

    added post game state which is entered immediately after the game has ended, responsible for displaying scoreboard and destroying objects/cleanup before return to menu

---
+++ Assets/Scripts/Game/Managers/Game Manager/Concrete States/PostGameState.cs
@@ -0,0 +1,34 @@
+﻿using ExtremeSnake.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.SceneManagement;
+
+namespace ExtremeSnake.Game
+{
+    public class PostGameState : BaseMonobehaviourState<GameManager>
+    {
+        public PostGameState(GameManager context) : base(context) { }
+        public override void TransitionTo() {
+            Subscriptions.Add(_context.GameEmitter.Subscribe("OnEnter",GameOver));
+            Subscriptions.Add(_context.GameEmitter.Subscribe("OnFinish",GameOver));
+            _context.GameEmitter.Emit("OnPostGame",this);
+        }
+
+        public override void FixedUpdate() { }
+
+        public override void LateUpdate() { }
+
+        public override void Update() { }
+
+        public void GameOver(object sender) {
+            _context.GameEmitter.Emit("OnGameOver",this);
+            _context.GameEmitter.UnsubscribeFromAll();
+            _context.Settings.ActiveSession.EndSession();
+            SceneManager.LoadScene(0);
+            _context.ChangeState(new MenuState(_context));
+        }
+    }
+}
