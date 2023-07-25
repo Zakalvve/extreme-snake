fedb9af94c35f0ce4981d502052280b8458c182f
Fohristiwhirl-Skaffen-Amtiskaw <112807626+Fohristiwhirl-Skaffen-Amtiskaw@users.noreply.github.com>
2023-07-25 17:56:56 +0100

    monobehaviour states now track which events they are subscribed to and can quickly unsubscribe when changing state

--- Assets/Scripts/Core/State/BaseMonobehaviourState.cs
+++ Assets/Scripts/Core/State/BaseMonobehaviourState.cs
@@ -1,14 +1,22 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace ExtremeSnake.Core
 {
     public abstract class BaseMonobehaviourState<T> : IMonobehaviourState where T: MonoBehaviour, IStateful<IMonobehaviourState>
     {
         protected T _context;
+        protected List<Action> Subscriptions = new List<Action>();
         public BaseMonobehaviourState(T context) {
             _context = context;
         }
 
+        protected void UnsubscribeFromAll() {
+            foreach (Action unsub in Subscriptions) {
+                unsub();
+            }
+        }
         public abstract void Update();
         public abstract void FixedUpdate();
         public abstract void LateUpdate();
