12967feca1c7a4224c32328d52414b1f19894d2f
Fohristiwhirl-Skaffen-Amtiskaw <112807626+Fohristiwhirl-Skaffen-Amtiskaw@users.noreply.github.com>
2023-07-26 20:29:43 +0100

    added package for serializing dictionaries in the unity editor

---
+++ Assets/Scripts/Core/SerializableDictionary/SDictionaryStringAudioClip.cs
@@ -0,0 +1,9 @@
+﻿using RotaryHeart.Lib.SerializableDictionary;
+using System;
+using UnityEngine;
+
+namespace ExtremeSnake.Core
+{
+    [Serializable]
+    public class SDictionaryStringAudioClip : SerializableDictionaryBase<string, AudioClip>  { }
+}
