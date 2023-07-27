fac9fce5399b1731795880a6b19540bcdfbddf84
Fohristiwhirl-Skaffen-Amtiskaw <112807626+Fohristiwhirl-Skaffen-Amtiskaw@users.noreply.github.com>
2023-07-27 21:30:47 +0100

    class for passing a game object as an argument to events

---
+++ Assets/Scripts/Game/Data/EventArgs/GameObjectEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace ExtremeSnake.Game.Data
+{
+    public class GameObjectEventArgs : EventArgs
+    {
+        public GameObject GO;
+        public GameObjectEventArgs(GameObject go) {
+            GO = go;
+        }
+    }
+}
